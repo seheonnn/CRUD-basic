47663e1a176b44de58d2954cc15a37efafb5a087
seheonnn <ho78901@naver.com>
2023-11-01 15:10:34 +0900

    Role update

--- src/main/java/com/example/CRUDbasic/config/RoleType.java
+++ src/main/java/com/example/CRUDbasic/config/RoleType.java
@@ -1,8 +1,21 @@
 package com.example.CRUDbasic.config;
 
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
-@Getter
+@RequiredArgsConstructor
 public enum RoleType {
-    USER, MANAGER
+
+    USER(0, "USER"),
+    MANAGER(1, "MANAGER");
+
+    private final Integer value;
+    private final String role;
+
+
+    @JsonValue
+    public String getRole() {
+        return role;
+    }
 }
---
+++ src/main/java/com/example/CRUDbasic/dto/UserReq.java
@@ -0,0 +1,4 @@
+package com.example.CRUDbasic.dto;
+
+public class UserReq {
+}
